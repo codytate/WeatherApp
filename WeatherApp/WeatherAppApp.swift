fc462b9861ca154a590b53241a45b464e5ae239a
Cody Tate <codyq.tate@gmail.com>
2025-11-03 19:44:36 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Cody Tate on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
